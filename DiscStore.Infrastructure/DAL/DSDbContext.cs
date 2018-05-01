1d83a2e92181c134a1e283c009f6b1d10f9a0afe
Maksymilian <maksymilian.matula@gmail.com>
2018-05-01 14:30:30 +0200

    Connection string and datacontext class

---
+++ DiscStore.Infrastructure/DAL/DSDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscStore.Infrastructure.DAL
+{
+    public class DSDbContext : DbContext
+    {
+        public DSDbContext() : base("DSDbContext")
+        {
+        }
+
+        //DbSet - here
+    }
+}
