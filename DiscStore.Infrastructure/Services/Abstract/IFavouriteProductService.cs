b5ad214b63307dbcb28df691ef2318d5b58ef9ac
maxmatula <maksymilian.matula@gmail.com>
2018-05-08 16:17:35 +0200

    favourite service

---
+++ DiscStore.Infrastructure/Services/Abstract/IFavouriteProductService.cs
@@ -0,0 +1,16 @@
+﻿using DiscStore.Infrastructure.ViewModels.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscStore.Infrastructure.Services.Abstract
+{
+    public interface IFavouriteProductService
+    {
+        bool AddToFavourites(Guid productId, string userId);
+        bool RemoveFromFavourites(Guid favouriteId);
+        List<ProductViewModel> FavouritesProductList(string userId);
+    }
+}
