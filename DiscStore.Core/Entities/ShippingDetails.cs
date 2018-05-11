d47ad7ed9e7f336f8fa77f2f2e8b03aef776dfd9
Maksymilian <maksymilian.matula@gmail.com>
2018-05-12 00:15:20 +0200

    Partial image croop by croppie.js TO do: upload cropped image

---
+++ DiscStore.Core/Entities/ShippingDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscStore.Core.Entities
+{
+    public class ShippingDetails
+    {
+        public string Name { get; set; }
+
+    }
+}
